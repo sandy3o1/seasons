272c234176154f450a13cda1d977105459b14138
sandy3o1 <bansal4o4xxx@gmail.com>
2018-11-24 20:22:02 +0530

    Conditional rendering :tiger: :wolf:

--- src/index.js
+++ src/index.js
@@ -2,15 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  // constructor function is the very first function that is going to be called any time an instance of this class is created
+  // when we define the constructor method it's going to be automatically called with the prop's object
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    };
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
+      position => {
+        this.setState({
+          lat: position.coords.latitude
+        });
+      },
+      err =>
+        this.setState({
+          errorMessage: err.message
+        })
     );
-    return <div>Lattitude!</div>;
+  }
+
+  // React says we have to deine render method inside class component which return some jsx
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Lattitude: {this.state.lat} </div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
 // getCurrentPosition will take some amount of time while when we calling lattitude it will not there
+// state must be initilized when a component is created
